7a79f7bd84e943da781a37bd22a8e828f7063e83
Joe Heck <heckj@mac.com>
2019-06-12 16:21:38 -0700

    adding sample project from SwiftUI tutorial walkthrough

---
+++ SwiftUI-Notes/SwiftUI-Notes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Notes
+//
+//  Created by Joseph Heck on 6/12/19.
+//  Copyright © 2019 SwiftUI-Notes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
