2515110eba4937ec785fae7c8ee227ddb96ef8c9
Joe Heck <heckj@mac.com>
2019-07-10 12:31:26 -0700

    @Publisher bidng example and explanation into pattern doc

--- UIKit-Combine/ViewController.swift
+++ UIKit-Combine/ViewController.swift
@@ -14,33 +14,44 @@ enum APIFailureCondition: Error {
 }
 
 private struct GithubAPIUser: Decodable {
-    // A very *small* subset of the content available about a github API user
-    // for example: https://api.github.com/users/heckj
+    // A very *small* subset of the content available about
+    //  a github API user for example:
+    // https://api.github.com/users/heckj
     let login: String
     let public_repos: Int
     let avatar_url: String
 }
 
 private struct GithubAPI {
-    // NOTE(heckj): I've also seen this kind of API access object set up with with a class and static methods on the class.
-    // I don't know that there's a specific benefit to make this a value type/struct with a function on it.
+    // NOTE(heckj): I've also seen this kind of API access
+    // object set up with with a class and static methods on the class.
+    // I don't know that there's a specific benefit to make this a value
+    // type/struct with a function on it.
 
-    /// creates a one-shot publisher that provides a GithubAPI User object as the end result.
-    /// This method was specifically designed to return a list of 1 object, as opposed to the object itself to make it easier to distinguish a
-    /// "no user" result (empty list) representation that could be dealt with more easily in a Combine pipeline than an optional value.
-    /// The expected return types is a Publisher that returns either an empty list, or a list of one GithubAPUser, and with a failure return
-    /// type of Never, so it's suitable for recurring pipeline updates working with a @Published data source.
+    /// creates a one-shot publisher that provides a GithubAPI User
+    /// object as the end result. This method was specifically designed to
+    /// return a list of 1 object, as opposed to the object itself to make
+    /// it easier to distinguish a "no user" result (empty list)
+    /// representation that could be dealt with more easily in a Combine
+    /// pipeline than an optional value. The expected return types is a
+    /// Publisher that returns either an empty list, or a list of one
+    /// GithubAPUser, and with a failure return type of Never, so it's
+    /// suitable for recurring pipeline updates working with a @Published
+    /// data source.
     /// - Parameter username: username to be retrieved from the Github API
     static func retrieveGithubUser(username: String) -> AnyPublisher<[GithubAPIUser], Never> {
 
         if username.count < 3 {
             return Just([]).eraseToAnyPublisher()
-//            return Publishers.Empty<GithubAPIUser, Never>().eraseToAnyPublisher()
+            // return Publishers.Empty<GithubAPIUser, Never>()
+            //    .eraseToAnyPublisher()
         }
         let assembledURL = String("https://api.github.com/users/\(username)")
         let publisher = URLSession.shared.dataTaskPublisher(for: URL(string: assembledURL)!)
-            //Instance method 'flatMap(maxPublishers:_:)' requires the types 'Published<Value>.Publisher.Failure' (aka 'Never') and
-            //'URLSession.DataTaskPublisher.Failure' (aka 'URLError') be equivalent
+            //Instance method 'flatMap(maxPublishers:_:)' requires the types
+            // 'Published<Value>.Publisher.Failure' (aka 'Never') and
+            //'URLSession.DataTaskPublisher.Failure' (aka 'URLError')
+            // be equivalent
 //            .handleEvents(receiveSubscription: { _ in
 //                DispatchQueue.main.async {
 //                    self.activityIndicator.startAnimating()
@@ -67,12 +78,14 @@ private struct GithubAPI {
             }
             .catch { err in
                 // return Publishers.Empty<GithubAPIUser, Never>()
-                // ^^ when I originally wrote this method, I was returning a GithubAPIUser? optional,
-                // and then a GithubAPIUser without optional. I ended up converting this to return an empty
-                // list as the "error output replacement" so that I could represent that the current value
-                // requested didn't *have* a correct github API response.
-                // When I was returing a single specific type, using Publishers.Empty was a good way to do
-                // a "no data on failure" error capture scenario.
+                // ^^ when I originally wrote this method, I was returning
+                // a GithubAPIUser? optional, and then a GithubAPIUser without
+                // optional. I ended up converting this to return an empty
+                // list as the "error output replacement" so that I could
+                // represent that the current value requested didn't *have* a
+                // correct github API response. When I was returing a single
+                // specific type, using Publishers.Empty was a good way to do a
+                // "no data on failure" error capture scenario.
                 return Just([])
             }
             .eraseToAnyPublisher()
@@ -95,7 +108,8 @@ class ViewController: UIViewController {
     // username from the github_id_entry field, updated via IBAction
     @Published var username: String = ""
 
-    // github user retrieved from the API publisher. As it's updated, it is "wired" to update UI elements
+    // github user retrieved from the API publisher. As it's updated, it
+    // is "wired" to update UI elements
     @Published private var githubUserData: [GithubAPIUser] = []
 
     // publisher reference for this is $username, of type <String, Never>
@@ -115,24 +129,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         let usernameSub = $username
-            .throttle(for: 0.5, scheduler: myBackgroundQueue, latest: true) // scheduler myBackGroundQueue publishes resulting elements into that queue...
+            .throttle(for: 0.5, scheduler: myBackgroundQueue, latest: true)
+            // ^^ scheduler myBackGroundQueue publishes resulting elements
+            // into that queue, resulting on this processing moving off the
+            // main runloop.
             .removeDuplicates()
-            .print("username pipeline: ")
+            .print("username pipeline: ") // debugging output for pipeline
             .map { username -> AnyPublisher<[GithubAPIUser], Never> in
                 return GithubAPI.retrieveGithubUser(username: username)
             }
-            // type returned in the pipeline is a Publisher, so we use switchToLatest to flatten the values out of that
-            // pipline to return down the chain, rather than returning a publisher down the pipeline.
+            // ^^ type returned in the pipeline is a Publisher, so we use
+            // switchToLatest to flatten the values out of that
+            // pipline to return down the chain, rather than returning a
+            // publisher down the pipeline.
             .switchToLatest()
-            // using a sink to get the results from the API search lets us get not only
-            // the user, but also any errors attempting to get it.
+            // using a sink to get the results from the API search lets us
+            // get not only the user, but also any errors attempting to get it.
             .receive(on: RunLoop.main)
             .sink { someValue in
                 self.githubUserData = someValue
             }
         usernameSubscriber = AnyCancellable(usernameSub)
 
-        // using .assign() on the other hand (which returns an AnyCancellable) *DOES* require a Failure type of <Never>
+        // using .assign() on the other hand (which returns an
+        // AnyCancellable) *DOES* require a Failure type of <Never>
         repositoryCountSubscriber = $githubUserData
             .print("github user data: ")
             .map { userData -> String in
@@ -145,21 +165,23 @@ class ViewController: UIViewController {
             .assign(to: \.text, on: repositoryCountLabel)
 
         let avatarViewSub = $githubUserData
-            // When I first wrote this publisher pipeline, the type I was aiming for was <GithubAPIUser?, Never>,
-            // where the value was an optional. The commented out .filter below was to prevent a `nil` GithubAPIUser
-            // object from propogating further and attempting to invoke the dataTaskPublisher which retrieves the
-            // avatar image.
+            // When I first wrote this publisher pipeline, the type I was
+            // aiming for was <GithubAPIUser?, Never>, where the value was an
+            // optional. The commented out .filter below was to prevent a `nil` // GithubAPIUser object from propogating further and attempting to
+            // invoke the dataTaskPublisher which retrieves the avatar image.
             //
-            // When I updated the type to be non-optional (<GithubAPIUser?, Never>) the filter expression was no
-            // longer needed, but possibly interesting.
+            // When I updated the type to be non-optional (<GithubAPIUser?,
+            // Never>) the filter expression was no longer needed, but possibly
+            // interesting.
             // .filter({ possibleUser -> Bool in
             //     possibleUser != nil
             // })
-            // .print("avatar image for user") // debugging output showing me pipeline interactions
+            // .print("avatar image for user") // debugging output
             .map { userData -> AnyPublisher<UIImage, Never> in
                 guard let firstUser = userData.first else {
-                    // my placeholder data being returned below is an empty UIImage() instance, which
-                    // simply clears the display. Your use case may be better served with an explicit
+                    // my placeholder data being returned below is an empty
+                    // UIImage() instance, which simply clears the display.
+                    // Your use case may be better served with an explicit
                     // placeholder image in the event of this error condition.
                     return Just(UIImage()).eraseToAnyPublisher()
                 }
@@ -170,34 +192,42 @@ class ViewController: UIViewController {
                     .map { UIImage(data: $0)!}
                     // ^^ convert Data into a UIImage with its initializer
                     .subscribe(on: self.myBackgroundQueue)
-                    // ^^ do this work on a background Queue so we don't screw with the UI responsiveness
+                    // ^^ do this work on a background Queue so we don't screw
+                    // with the UI responsiveness
                     .catch { err in
                         return Just(UIImage())
                     }
-                    // ^^ deal the failure scenario and return my "replacement" image for when an avatar image
-                    // either isn't available or fails somewhere in the pipeline here.
+                    // ^^ deal the failure scenario and return my "replacement"
+                    // image for when an avatar image either isn't available or
+                    // fails somewhere in the pipeline here.
                     .eraseToAnyPublisher()
-                    // ^^ match the return type here to the return type defined in the .map() wrapping this
-                    // because otherwise the return type would be terribly complex nested set of generics.
+                    // ^^ match the return type here to the return type defined
+                    // in the .map() wrapping this because otherwise the return
+                    // type would be terribly complex nested set of generics.
             }
             .switchToLatest()
-            // ^^ Take the returned publisher that's been passed down the chain and "subscribe it out" to the value
-            // within in, and then pass that further down.
+            // ^^ Take the returned publisher that's been passed down the chain
+            // and "subscribe it out" to the value within in, and then pass
+            // that further down.
             .subscribe(on: myBackgroundQueue)
-            // ^^ do the above processing as well on a background Queue rather than potentially impacting the UI responsiveness
+            // ^^ do the above processing as well on a background Queue rather
+            // than potentially impacting the UI responsiveness
             .receive(on: RunLoop.main)
-            // ^^ and then switch to receive and process the data on the main queue since we're messin with the UI
+            // ^^ and then switch to receive and process the data on the main
+            // queue since we're messin with the UI
 
             // .assign(to: \.image, on: self.githubAvatarImageView)
-            // this ^^^ line is returning a compiler error: Type of expression is ambiguous without more context
-            // I *thought* it would work, but it's having an issue with the keyPath that I'm trying to assign
-            // for the githubAvatarImageView.image.
+            // this ^^^ line is returning a compiler error: Type of expression
+            // is ambiguous without more context. I *thought* it would work,
+            // but it's having an issue with the keyPath that I'm trying to
+            // assign for the githubAvatarImageView.image.
 
             // so instead we can use a sink to capture the data and set a value
             .sink(receiveValue: { image in
                 self.githubAvatarImageView.image = image
             })
-        // convert the .sink to an `AnyCancellable` object that we have referenced from the implied initializers
+        // convert the .sink to an `AnyCancellable` object that we have
+        // referenced from the implied initializers
         avatarViewSubscriber = AnyCancellable(avatarViewSub)
     }
 
