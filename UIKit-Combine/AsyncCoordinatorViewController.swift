0213dc8e337a35d3234a5d95d750cec4d902323b
Joe Heck <heckj@mac.com>
2019-07-21 14:05:46 -0700

    starting to create pipeline

--- UIKit-Combine/AsyncCoordinatorViewController.swift
+++ UIKit-Combine/AsyncCoordinatorViewController.swift
@@ -13,7 +13,6 @@ class AsyncCoordinatorViewController: UIViewController {
 
     @IBOutlet weak var startButton: UIButton!
 
-
     @IBOutlet weak var step1_button: UIButton!
     @IBOutlet weak var step2_1_button: UIButton!
     @IBOutlet weak var step2_2_button: UIButton!
@@ -22,12 +21,95 @@ class AsyncCoordinatorViewController: UIViewController {
     @IBOutlet weak var step4_button: UIButton!
 
     @IBAction func startButtonPressed(_ sender: UIButton) {
+
     }
-    
+
+    var coordinatedPipeline: AnyPublisher<Bool, Error>?
+
+    // MARK: - helper pieces that would normally be in other files
+
+    // this emulates an async API call with a completion callback
+    // it does nothing other than wait and ultimately return with a boolean value
+    func randomAsyncAPI(completion completionBlock: @escaping ((Bool, Error?) -> Void)) {
+        DispatchQueue.global(qos: .background).async {
+            sleep(.random(in: 2...5))
+            completionBlock(true, nil)
+        }
+    }
+
+    func createFuturePublisher() -> AnyPublisher<Bool, Error> {
+        return Future<Bool, Error> { promise in
+            self.randomAsyncAPI() { (result, err) in
+                if let err = err {
+                    promise(.failure(err))
+                }
+                promise(.success(result))
+            }
+        }.eraseToAnyPublisher()
+    }
+
+    func markStepDone(button: UIButton) {
+        button.backgroundColor = .systemGreen
+        button.isHighlighted = true
+    }
+
+    // MARK: - view setup
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        coordinatedPipeline = createFuturePublisher()
+            .receive(on: RunLoop.main) // convenience to make it easy to tweak the UI to display progress
+            .map { _ -> Bool in
+                // intentially side effecting here to show progress of pipeline
+                self.markStepDone(button: self.step1_button)
+                return true
+            }
+        .flatMap { _ in
+            return self.createFuturePublisher()
+            //            let step2_1 = self.createFuturePublisher()
+//                .receive(on: RunLoop.main) // convenience to make it easy to tweak the UI to display progress
+//                .map {
+//                    // intentially side effecting here to show progress of pipeline
+//                    self.markStepDone(button: self.step2_1_button)
+//                    return $0
+//                }
+//
+//            let step2_2 = self.createFuturePublisher()
+//                .receive(on: RunLoop.main) // convenience to make it easy to tweak the UI to display progress
+//                .map {
+//                    // intentially side effecting here to show progress of pipeline
+//                    self.markStepDone(button: self.step2_2_button)
+//                    return $0
+//                }
+//
+//            let step2_3 = self.createFuturePublisher()
+//                .receive(on: RunLoop.main) // convenience to make it easy to tweak the UI to display progress
+//                .map {
+//                    // intentially side effecting here to show progress of pipeline
+//                    self.markStepDone(button: self.step2_3_button)
+//                    return $0
+//                }
+//
+//            return Publishers.Zip3(step2_1, step2_2, step2_3)
+//                .map { _ -> Bool in
+//                    return true
+//                }.eraseToAnyPublisher()
+
+            }
+        .eraseToAnyPublisher() as AnyPublisher<Bool, Error>
+
+
+//        // driving it by attaching it to .sink
+//        let cancellable = futurePublisher.sink(receiveCompletion: { err in
+//            print(".sink() received the completion: ", String(describing: err))
+//            expectation.fulfill()
+//        }, receiveValue: { value in
+//            print(".sink() received value: ", value)
+//            outputValue = value
+//        })
+
     }
     
 
