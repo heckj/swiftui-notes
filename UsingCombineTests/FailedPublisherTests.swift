6b7455f0c2e3db391dae5139ce2ef3438e7fa007
Joe Heck <heckj@mac.com>
2019-08-11 11:48:18 -0700

    fail publisher

--- UsingCombineTests/FailedPublisherTests.swift
+++ UsingCombineTests/FailedPublisherTests.swift
@@ -11,4 +11,32 @@ import Combine
 
 class FailedPublisherTests: XCTestCase {
 
+    enum testFailureCondition: Error {
+        case exampleFailure
+    }
+
+    func testFailPublisher() {
+        let expectation = XCTestExpectation(description: self.debugDescription)
+
+        let cancellable = Fail<String, Error>(error: testFailureCondition.exampleFailure)
+            .sink(receiveCompletion: { completion in
+                print(".sink() received the completion", String(describing: completion))
+                switch completion {
+                case .finished:
+                    XCTFail("No failure should be received from empty")
+                    break
+                case .failure(let anError):
+                    print("received error: ", anError)
+                    break
+                }
+                expectation.fulfill()
+            }, receiveValue: { postmanResponse in
+                XCTFail("No vaue should be received from empty")
+                print(".sink() data received \(postmanResponse)")
+            })
+
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(cancellable)
+    }
+
 }
