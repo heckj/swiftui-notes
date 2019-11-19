b319216f34d8159e7efdb4b31c2767e521051f9a
Joseph Heck <heckj@mac.com>
2019-11-19 09:05:28 -0800

    update the retry docs re: optional count value (no longer correct) (#123)
    * update the retry docs re: optional count value (no longer correct)
    
    * significant rewrite to better explain the code and example in the pattern-retry
    
    * text cleanup
    
    * create example test working future and retry together
    
    * feedback posted about Future and retry not working and playing well together
    
    * add Future/retry incompatibility warning into Future reference section

--- UsingCombineTests/FuturePublisherTests.swift
+++ UsingCombineTests/FuturePublisherTests.swift
@@ -15,10 +15,12 @@ class FuturePublisherTests: XCTestCase {
         case anErrorExample
     }
 
-    // example of a functional calling it with a completion closure
+    // example of a asynchronous function to be called from within a Future and its completion closure
     func asyncAPICall(sabotage: Bool, completion completionBlock: @escaping ((Bool, Error?) -> Void)) {
         DispatchQueue.global(qos: .background).async {
-            sleep(.random(in: 1...3))
+            let delay = Int.random(in: 1...3)
+            print(" * making async call (delay of \(delay) seconds)")
+            sleep(UInt32(delay))
             if sabotage {
                 completionBlock(false, TestFailureCondition.anErrorExample)
             }
@@ -68,6 +70,63 @@ class FuturePublisherTests: XCTestCase {
                 promise(.success(grantedAccess))
             }
         }
+
+        // driving it by attaching it to .sink
+        let cancellable = sut.sink(receiveCompletion: { err in
+            print(".sink() received the completion: ", String(describing: err))
+            XCTAssertNotNil(err)
+            expectation.fulfill()
+        }, receiveValue: { value in
+            print(".sink() received value: ", value)
+            XCTFail("no value should be returned")
+        })
+
+        wait(for: [expectation], timeout: 5.0)
+        XCTAssertNotNil(cancellable)
+    }
+
+    func testFuturePublisherShowingFailureWithRetry() {
+        // setup
+        let expectation = XCTestExpectation(description: self.debugDescription)
+
+        // the creating the future publisher
+        let sut = Future<Bool, Error> { promise in
+            print("invoking Future handler for resolving the provided promise")
+            self.asyncAPICall(sabotage: true) { (grantedAccess, err) in
+                print("invoking async completion handler to return a resolved promise")
+                if let err = err {
+                    promise(.failure(err))
+                }
+                promise(.success(grantedAccess))
+            }
+        }
+        .print("before_retry:")
+        .retry(2)
+        .print("after_retry:")
+
+//        output from this test:
+//        invoking Future handler for resolving the provided promise
+//        before_retry:: receive subscription: (Future)
+//        after_retry:: receive subscription: (Retry)
+//        after_retry:: request unlimited
+//        before_retry:: request unlimited
+//         * making async call (delay of 1 seconds)
+//        invoking async completion handler to return a resolved promise
+//        before_retry:: receive error: (anErrorExample)
+//        before_retry:: receive subscription: (Future)
+//        before_retry:: request unlimited
+//        before_retry:: receive error: (anErrorExample)
+//        before_retry:: receive subscription: (Future)
+//        before_retry:: request unlimited
+//        before_retry:: receive error: (anErrorExample)
+//        after_retry:: receive error: (anErrorExample)
+//        .sink() received the completion:  failure(UsingCombineTests.FuturePublisherTests.TestFailureCondition.anErrorExample)
+
+        // NOTE(heckj): from this output, it appears that Future maintain's its internal state of any promise resolutions, and
+        // subsequent subscriiption/demand invocations to it as a publisher will not retrigger the provided closures. This
+        // implies that it's rather incompatible with the retry() operator.
+        //
+        // This has been reported to Apple as Feedback : FB7455914
 
         // driving it by attaching it to .sink
         let cancellable = sut.sink(receiveCompletion: { err in
