391bd8a24ce3fb9196b9b5a51e464ed3b6f003b1
Joe Heck <heckj@mac.com>
2019-11-19 08:14:58 -0800

    create example test working future and retry together

--- UsingCombineTests/FuturePublisherTests.swift
+++ UsingCombineTests/FuturePublisherTests.swift
@@ -15,10 +15,12 @@ class FuturePublisherTests: XCTestCase {
         case anErrorExample
     }
 
-    // example of a functional calling it with a completion closure
+    // example of a asynchronous function to be called from within a Future and its completion closure
     func asyncAPICall(sabotage: Bool, completion completionBlock: @escaping ((Bool, Error?) -> Void)) {
         DispatchQueue.global(qos: .background).async {
-            sleep(.random(in: 1...3))
+            let delay = Int.random(in: 1...3)
+            print(" * making async call (delay of \(delay) seconds)")
+            sleep(UInt32(delay))
             if sabotage {
                 completionBlock(false, TestFailureCondition.anErrorExample)
             }
@@ -83,6 +85,52 @@ class FuturePublisherTests: XCTestCase {
         XCTAssertNotNil(cancellable)
     }
 
+    func testFuturePublisherShowingFailureWithRetry() {
+        // setup
+        let expectation = XCTestExpectation(description: self.debugDescription)
+
+        // the creating the future publisher
+        let sut = Future<Bool, Error> { promise in
+            self.asyncAPICall(sabotage: true) { (grantedAccess, err) in
+                if let err = err {
+                    promise(.failure(err))
+                }
+                promise(.success(grantedAccess))
+            }
+        }
+        .print("before_retry:")
+        .retry(2)
+        .print("after_retry:")
+
+//        output from this test:
+//        before_retry:: receive subscription: (Future)
+//        after_retry:: receive subscription: (Retry)
+//        after_retry:: request unlimited
+//        before_retry:: request unlimited
+//         * making async call (delay of 1 seconds)
+//        before_retry:: receive error: (anErrorExample)
+//        before_retry:: receive subscription: (Future)
+//        before_retry:: request unlimited
+//        before_retry:: receive error: (anErrorExample)
+//        before_retry:: receive subscription: (Future)
+//        before_retry:: request unlimited
+//        before_retry:: receive error: (anErrorExample)
+//        after_retry:: receive error: (anErrorExample)
+//        .sink() received the completion:  failure(UsingCombineTests.FuturePublisherTests.TestFailureCondition.anErrorExample)
+
+        // driving it by attaching it to .sink
+        let cancellable = sut.sink(receiveCompletion: { err in
+            print(".sink() received the completion: ", String(describing: err))
+            XCTAssertNotNil(err)
+            expectation.fulfill()
+        }, receiveValue: { value in
+            print(".sink() received value: ", value)
+            XCTFail("no value should be returned")
+        })
+
+        wait(for: [expectation], timeout: 5.0)
+        XCTAssertNotNil(cancellable)
+    }
     func testFutureWithinAFlatMap() {
         let simplePublisher = PassthroughSubject<String, Never>()
         var outputValue: String? = nil
