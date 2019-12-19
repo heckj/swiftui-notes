25f7ca7879693663a3540b642bd7dbdbc8e8d18a
Joe Heck <heckj@mac.com>
2019-12-19 16:28:10 -0600

    criteria operator text content

--- UsingCombineTests/CriteriaOperatorTests.swift
+++ UsingCombineTests/CriteriaOperatorTests.swift
@@ -42,9 +42,47 @@ class CriteriaOperatorTests: XCTestCase {
         passSubj.send("hello")
         XCTAssertEqual(responses.count, 0)
         XCTAssertFalse(terminatedStream)
+        passSubj.send("world")
+        XCTAssertEqual(responses.count, 0)
+        XCTAssertFalse(terminatedStream)
         passSubj.send("abc")
         XCTAssertEqual(responses.count, 1)
         XCTAssertEqual(responses, [true])
+        XCTAssertTrue(terminatedStream)
+
+        XCTAssertNotNil(cancellable)
+    }
+
+    func testContainsFinished() {
+        let passSubj = PassthroughSubject<String, Never>()
+        // no initial value is propogated from a PassthroughSubject
+
+        var responses = [Bool]()
+        var terminatedStream = false
+
+        let cancellable = passSubj
+        .contains("abc")
+        .sink(receiveCompletion: { completion in
+            print(".sink() received the completion", String(describing: completion))
+            terminatedStream = true
+            switch completion {
+            case .finished:
+                break
+            case .failure(let anError):
+                print("received error: ", anError)
+                break
+            }
+        }, receiveValue: { responseValue in
+            responses.append(responseValue)
+            print(".sink() data received \(responseValue)")
+        })
+
+        passSubj.send("hello")
+        XCTAssertEqual(responses.count, 0)
+        XCTAssertFalse(terminatedStream)
+        passSubj.send(completion: Subscribers.Completion.finished)
+        XCTAssertEqual(responses.count, 1)
+        XCTAssertEqual(responses, [false])
         XCTAssertTrue(terminatedStream)
 
         XCTAssertNotNil(cancellable)
@@ -82,6 +120,43 @@ class CriteriaOperatorTests: XCTestCase {
         passSubj.send("abc")
         XCTAssertEqual(responses.count, 1)
         XCTAssertEqual(responses, [true])
+        XCTAssertTrue(terminatedStream)
+
+        XCTAssertNotNil(cancellable)
+    }
+
+    func testContainsWhereFinished() {
+        let passSubj = PassthroughSubject<String, Never>()
+        // no initial value is propogated from a PassthroughSubject
+
+        var responses = [Bool]()
+        var terminatedStream = false
+
+        let cancellable = passSubj
+        .contains { someval -> Bool in
+            return someval == "abc"
+        }
+        .sink(receiveCompletion: { completion in
+            print(".sink() received the completion", String(describing: completion))
+            terminatedStream = true
+            switch completion {
+            case .finished:
+                break
+            case .failure(let anError):
+                print("received error: ", anError)
+                break
+            }
+        }, receiveValue: { responseValue in
+            responses.append(responseValue)
+            print(".sink() data received \(responseValue)")
+        })
+
+        passSubj.send("hello")
+        XCTAssertEqual(responses.count, 0)
+        XCTAssertFalse(terminatedStream)
+        passSubj.send(completion: Subscribers.Completion.finished)
+        XCTAssertEqual(responses.count, 1)
+        XCTAssertEqual(responses, [false])
         XCTAssertTrue(terminatedStream)
 
         XCTAssertNotNil(cancellable)
