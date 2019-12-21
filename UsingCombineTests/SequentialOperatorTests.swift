f512cbb8efbf8b0f65c510cce04b386c46066676
Joe Heck <heckj@mac.com>
2019-12-21 13:21:58 -0600

    starting in on seq operator tests

---
+++ UsingCombineTests/SequentialOperatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  SequentialOperatorTests.swift
+//  UsingCombineTests
+//
+//  Created by Joseph Heck on 12/21/19.
+//  Copyright © 2019 SwiftUI-Notes. All rights reserved.
+//
+
+import XCTest
+import Combine
+
+class SequentialOperatorTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
