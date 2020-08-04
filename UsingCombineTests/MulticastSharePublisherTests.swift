a061150bbc56fbf6934eb6cd2b881cdb1f0709dc
Will Wheatley <will@iwill.id.au>
2020-08-04 23:17:02 +1000

    Hopefully I've conveyed a use-case for autoconnect(). Adding a subscriber to a chain without the regular subscribers of the publisher needing to be modified.

--- UsingCombineTests/MulticastSharePublisherTests.swift
+++ UsingCombineTests/MulticastSharePublisherTests.swift
@@ -261,36 +261,101 @@ class MulticastSharePublisherTests: XCTestCase {
 
     func testMakeConnectable() {
         // setup
-        let expectation1 = XCTestExpectation(description: self.debugDescription)
-        let expectation2 = XCTestExpectation(description: self.debugDescription)
+        let firstCompletion = expectation(description: self.debugDescription)
+
+        let values = expectation(description: self.debugDescription)
+        values.expectedFulfillmentCount = 4
+
+        let waiting = expectation(description: self.debugDescription)
+
         var cancellables = Set<AnyCancellable>()
 
-        let publisher = Just("woot")
+        let publisher = [1,2].publisher
             .makeConnectable()
 
         // driving it by attaching it to .sink
         publisher.sink(receiveCompletion: { completion in
             print(".sink1() received the completion: ", String(describing: completion))
-            expectation1.fulfill()
+            firstCompletion.fulfill()
         }, receiveValue: { value in
             print(".sink1() received value: ", value)
+            values.fulfill()
         })
         .store(in: &cancellables)
+
+        // Setup the order in our wait. The first completion won't have ooccured yet.
+        waiting.fulfill()
+
+        let autoPublisher = publisher.autoconnect()
 
         // driving it by attaching it to .sink
-        publisher.sink(receiveCompletion: { completion in
+        autoPublisher.sink(receiveCompletion: { completion in
             print(".sink2() received the completion: ", String(describing: completion))
-            expectation2.fulfill()
         }, receiveValue: { value in
             print(".sink2() received value: ", value)
+            values.fulfill()
         })
         .store(in: &cancellables)
 
-        publisher
-            .connect()
-            .store(in: &cancellables)
-
-        wait(for: [expectation1, expectation2], timeout: 5.0)
+        // We should have fulfilled our mock "setup" before anyone received values
+        wait(for: [waiting, values, firstCompletion], timeout: 5.0, enforceOrder: true)
     }
 
+    // As the makeConnectable example above, but now with MultiCast we can include an error
+    // Setup a "pipeline" inspector before handing the publisher back to the unsuspecting
+    // original subscriber
+
+    func testMulticastDeferredFutureAutoConnectPublisher() {
+        // setup
+        let doSomeSpyWork = expectation(description: self.debugDescription)
+        let legitCompletion = expectation(description: self.debugDescription)
+        let spyCompletion = expectation(description: self.debugDescription)
+        let spyValueReceived = expectation(description: self.debugDescription)
+        let legitValueReceived = expectation(description: self.debugDescription)
+
+        var cancellables = Set<AnyCancellable>()
+
+        // the creating the deferred, future publisher
+        let publisher = Deferred {
+            Future<Int, Error> { promise in
+                self.asyncAPICall(sabotage: false) { (grantedAccess, err) in
+                    if let err = err {
+                        promise(.failure(err))
+                    } else {
+                        promise(.success(grantedAccess))
+                    }
+                }
+            }
+        }.multicast {
+            // alternate way of using multicast that creates the relevant subject inline
+            PassthroughSubject<Int, Error>()
+        }
+
+        // Attach our 'spy' subscriber. The publisher won't receive a 'send' .... yet
+        publisher.sink(receiveCompletion: { completion in
+            print(".sink() received the completion: ", String(describing: completion))
+            spyCompletion.fulfill()
+        }, receiveValue: { value in
+            print(".sink() received value: ", value)
+            spyValueReceived.fulfill()
+        })
+        .store(in: &cancellables) // Note: the spy needs to keep a reference to his own subscriber
+
+        // Our spy has some work to do now before hands back the intercepted publisher
+        doSomeSpyWork.fulfill()
+
+        // Now hand off the publisher to the real subscriber
+        let spyedPublisher = publisher.autoconnect()
+        spyedPublisher.sink(receiveCompletion: { completion in
+            print(".sink() received the completion: ", String(describing: completion))
+            legitCompletion.fulfill()
+        }, receiveValue: { value in
+            print(".sink() received value: ", value)
+            legitValueReceived.fulfill()
+        })
+        .store(in: &cancellables)
+
+        wait(for: [doSomeSpyWork, spyValueReceived], timeout: 5.0, enforceOrder: true)
+        wait(for: [legitValueReceived, legitCompletion, spyCompletion], timeout: 5.0)
+    }
 }
