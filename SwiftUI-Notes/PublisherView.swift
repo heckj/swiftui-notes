9ee3b96861b9f906a887818200f8d3691254c00c
Joe Heck <heckj@mac.com>
2021-02-07 09:58:25 -0800

    fixing share details, adding example code

---
+++ SwiftUI-Notes/PublisherView.swift
@@ -0,0 +1,44 @@
+//
+//  PublisherView.swift
+//  SwiftUI-Notes
+//
+//  Created by Joseph Heck on 2/7/21.
+//  Copyright © 2021 SwiftUI-Notes. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+struct PublisherBindingExampleView: View {
+    
+    @State private var filterText = ""
+    @State private var delayed = ""
+    
+    private var relay = PassthroughSubject<String,Never>()
+    private var debouncedPublisher: AnyPublisher<String, Never>
+    
+    init() {
+        self.debouncedPublisher = relay
+            .debounce(for: 1, scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+    var body: some View {
+        VStack {
+            TextField("filter", text: $filterText)
+                .onChange(of: filterText, perform: { value in
+                    relay.send(value)
+                })
+            Text("Delayed result: \(delayed)")
+                .onReceive(debouncedPublisher, perform: { value in
+                    delayed = value
+                })
+        }
+    }
+}
+
+struct PublisherView_Previews: PreviewProvider {
+    static var previews: some View {
+        PublisherView()
+    }
+}
