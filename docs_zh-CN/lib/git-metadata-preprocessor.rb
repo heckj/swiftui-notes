06c0a354c52e81db72e8a76130ad1c9c672447c2
Joe Heck <heckj@mac.com>
2021-12-21 08:04:34 -0800

    removing duplicate files, updating HTML publishing for dual languages

--- docs_zh-CN/lib/git-metadata-preprocessor.rb
+++
@@ -1,7 +0,0 @@
-RUBY_ENGINE == 'opal' ?
-  (require 'git-metadata-preprocessor/extension') :
-  (require_relative 'git-metadata-preprocessor/extension')
-
-Asciidoctor::Extensions.register do
-  preprocessor GitMetadataPreprocessor
-end
